7624626a5feb51bab58c1f8ac4b9d0742341532b
Guilherme0204 <gui2605costa@gmail.com>
2023-07-11 19:54:06 -0300

    Add Css

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+const form = document.getElementById("contact-form");
 
-    const form = document.getElementById("contact-form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
+    const name = form.elements["nome"].value;
+    const email = form.elements["email"].value;
+    const message = form.elements["mensagem"].value;
+    const result = `Nome: ${name}\nE-mail: ${email}\nMensagem: ${message}`;
+    if (name.length === 0 && email.length === 0 && message.length === 0) {
+        alert("Todos os campos são necessários para continuar");
 
-        const name = form.elements["nome"].value;
-        const email = form.elements["email"].value;
-        const message = form.elements["mensagem"].value;
-        if(name.length === 0){
-            alert("Nome necessário")
+    } else {
+
+        if (name.length === 0 && email.length > 0 && message.length > 0) {
+            alert("Nome Necessário para continuar");
+        } else if (name.length > 0 && email.length === 0 && message.length > 0) {
+            alert("Informe o email para continuar");
+        } else if (name.length > 0 && email.length > 0 && message.length === 0) {
+            alert("Informe a mensagem para continuar");
+        } else if (name.length === 0 || email.length === 0 || message.length === 0) {
+            alert("Todos os campos são necessários para continuar");
+
+        } else {
+            alert(result);
         }
 
-        const result = `Nome: ${name}\nE-mail: ${email}\nMensagem: ${message}`;
+    }
 
-        alert(result);
-    });
+
+});
